5c9aabf13f18695e7bec061519f339b08f03dc6f
LopesACF7 <altemilsonlopes@gmail.com>
2023-03-31 19:48:09 -0300

    Exercicio Finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Olá, Minhas frutas preferidas são:");
+
+Console.WriteLine("Manga...");
+Thread.Sleep(1000);
+Console.WriteLine("Uva...");
+Thread.Sleep(1000);
+Console.WriteLine("Acerola...");
+Thread.Sleep(1000);
+Console.WriteLine("Caqui...");
+Thread.Sleep(1000);
+Console.WriteLine("Laranja.");
